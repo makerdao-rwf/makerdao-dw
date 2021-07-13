50d5c1a4583074c37298d3276471f2a636b54ee9
Joe Delistraty <delistraty@gmail.com>
2021-07-13 13:43:19 -0700

    Added Contracts, auto-blockstep, etc. Working on proxy_actions (incomplete)

--- eth-contract.py
+++ eth-contract.py
@@ -19,7 +19,7 @@ import warnings
 import requests
 import time
 
-from functions import get_abi
+from functions import get_abi, get_abi_params
 
 
 # Allow user to input contract in terminal command
@@ -35,10 +35,14 @@ conf = ConfigFactory.parse_file('config.conf')
 
 ## Environment parameters
 infura_key = conf["infura_key"]
-fromBlock = conf["contracts"][schema][contract_name]["creationBlock"]
-#fromBlock = 12736124
+try:
+  fromBlock = conf["contracts"][schema][contract_name]["creationBlock"]
+except:
+  print("Did you add the file path to config.conf?")
+  raise
 
-# Number of block per call (too big can lead to errors, too low can be too long)
+
+# Number of blocks per call (too big can lead to errors, too low can be too long)
 blocksStep = conf.get(f"contracts.{schema}.{contract_name}.blocksStep", conf["blocksStep"])
 
 ## Input parameters
@@ -48,88 +52,51 @@ db_password = conf["db.password"]
 db_db = conf["db.database"]
 addresses = conf["contracts"][schema][contract_name]["addresses"]
 print(addresses)
-
-
-# I think you have to put this under 'for address in addresses' below. 
-for address in addresses: # This only gets the last address. Get all
-  address, abi, contract_name = get_abi(address, schema, contract_name)
   
 # Connect to PostgreSQL
 engine = create_engine('postgresql://'+db_user+':'+db_password+'@'+db_host+':5432/'+db_db) 
 
 # Set w3 source to Infura Mainnet and init contract
 w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/' + infura_key)) 
-addresses = [w3.toChecksumAddress(a) for a in addresses]
-contract = w3.eth.contract(address=addresses[0], abi=abi)
 
-# add tablename to abi json 
-dict_fn = {} ## Manage an index for disambuguation of functions with same names but different signature
-dict_evt = {} ## Manage an index for disambuguation of events  with same names but different signature
-dict_sign = {} # function /events signature to item (function or event) abi
-
-
+# Convert each event's input data to a readable format. Then decode it.
 def get_function_data(t):
-  # Convert each event's input data to a readable format. Try multiples of 2 + 32: e.g. 34, 66, 98, 130, etc.
   x=2
   inputs = None
 
-  while inputs is None:
-    try:
-      #print(t['transactionHash'])
-      input_data = '0x' + t['data'][x:]
-      inputs = contract.decode_function_input(input_data)
-      params = inputs[1].values()
-      #print(inputs)
-      
-    except ValueError:    #If you get the 'could not find any function with matching selector' error, then the input data is incorrectly formatted for 'decode_function_input'. This may happen when there are too many topic fields. Thise truncates the input data to make it accepted by the decode_input_function.
-      x = x+32 #or x=x+1 if that doesn't work?
-      #print(input_data)
+  if t['data'] == '0x':
+    print("The input data coming from Infura is empty.")
+    inputs = []
+    params =[]
 
-      if input_data == '0x': #If the string is never able to be read 'decode_function_input' (and it just truncates to 0x)
-        print('Cannot read input data. The following input data may be invalid.' + t['data'])
-        x=2
-        #inputs = 1
+  else:
+    while inputs is None:
+      try:
+        #print(t['transactionHash'])
+        input_data = '0x' + t['data'][x:]
+        inputs = contract.decode_function_input(input_data)
+        params = inputs[1].values()
+        #print(inputs)
         
-      pass
+      except ValueError:    
+        x = x + 8 #or x+=32 or x+=8. NOTE: This removes leading topics (0s) from the input data. Works well in multiples of 8 or 16.
+        
+        if input_data == '0x': #If the string is never able to be read 'decode_function_input' (and it just truncates to 0x)
+          print('Cannot read input data. The input data or ABI may be invalid.', t['data'])
+          x=2
+          # Do I need to raise an error here?
+          #inputs = []
+          #params = []
+
+        pass
       
   return inputs, params
-
-#From the ABI, get function and event names j['name']
-def get_abi_params(abi):
-  for j in abi:
-    if j["type"] == "function" and j["stateMutability"] != "view":
-      fn_name = j["name"].lower() 
-      signature = '{}({})'.format(j['name'],','.join([input['type'] for input in j['inputs']]))
-      # Functions signature use the 4 first bytes of the sha3 then 0
-      j["signature"] = w3.sha3(text=signature)[0:4].hex() + '00000000000000000000000000000000000000000000000000000000'
-      # print(f"{j['name']}   {signature}   {j['signature']}")
-      # If the name already exists, we add an index starting by 0 at the end of the function
-      if fn_name in dict_fn:
-        j["table"] = contract_name + "_call_" + fn_name + str(dict_fn[fn_name])
-        dict_fn[fn_name] = dict_fn[fn_name]+1
-      else:
-        j["table"] = contract_name + "_call_" + fn_name
-        dict_fn[fn_name] = 0
-      dict_sign[j["signature"]] = j
-    elif j["type"] == "event" and j["anonymous"] != True:
-      j["signature"] = eth_event.get_log_topic(j)
-      fn_name = j["name"].lower()
-      # If the name already exists, we add an index starting by 0 at the end of the function
-      if fn_name in dict_evt:
-        j["table"] = contract_name + "_evt_" + fn_name + str(dict_evt[fn_name])
-        dict_evt[fn_name] = dict_evt[fn_name]+1
-      else:
-        j["table"] = contract_name + "_evt_" + fn_name
-        dict_evt[fn_name] = 0
-      dict_sign[j["signature"]] = j
-  return j
-
-# Initialize
+  
 
 def create_schema (abi):
   # create all tables if needed
   common_columns = "block_number bigint, block_hash bytea, address bytea, log_index int, transaction_index int, transaction_hash bytea"
-  type_mapping = {"address": "bytea", "bytes": "bytea", "bytes4": "bytea", "bytes32": "bytea", "int256": "numeric", "uint256": "numeric", "uint16":"numeric", "bool": "boolean", "address[]":"bytea", "uint256[]":"numeric"}
+  type_mapping = {"address": "bytea", "bytes": "bytea", "bytes4": "bytea", "bytes32": "bytea", "int256": "numeric", "uint256": "numeric", "uint16":"numeric", "bool": "boolean", "address[]":"bytea", "uint256[]":"numeric", "uint8":"numeric", "string":"bytea"}
 
   with engine.connect() as sql:
     sql.execute(text(f"create schema if not exists {schema}")) 
@@ -157,27 +124,50 @@ def create_schema (abi):
           print(sql_create_table)
           sql.execute(text(sql_create_table))
 
-# Get ABI parameters
-j = get_abi_params(abi)
-#print('this is j', j, j['table'], j['type'], j['name'])
 
-# Create SQL Schema (if it doesn't already exist)
-create_schema(abi)
+# Return transaction logs or filter the specific type of log you need to return.
+def read_logs(address, fromBlock, toBlock):   
+
+  # If we're finding events from DSSProxyActions (x82ecd135dce65fbc6dbdd0e4237e0af93ffd5038)
+  if contract_name == 'proxy_actions':
+    t = []
+    for logs in w3.eth.get_logs({'fromBlock': fromBlock, 'toBlock': lastBlock, 'address': address}):
+      try:  
+        if '82ecd135dce65fbc6dbdd0e4237e0af93ffd5038' in logs.topics[2].hex():
+          t.append(logs)
+      except:
+        pass
+    return t
+      
+  #If we're not finding events from proxy_actions, just read logs normally
+  else:
+    t = w3.eth.get_logs({'fromBlock': fromBlock, 'toBlock': toBlock, 'address': address})
+    return t
+
+
+# Get addresses, contract names, and ABI parameters
+for address in addresses: # This only gets the last address. Get all. I think you have to put this under 'for address in addresses' below. ***
+  address, abi, contract_name = get_abi(address, schema, contract_name)
+
+addresses = [w3.toChecksumAddress(a) for a in addresses] # Get addresses
+contract = w3.eth.contract(address=addresses[0], abi=abi) # Get contracts
+j, dict_evt, dict_fn, dict_sign = get_abi_params(abi, contract_name, w3) # Get ABI parameters (function names, event names, etc.) 
+create_schema(abi) # Create SQL Schema if it doesn't already exist
+
+# Start Reading transactions
+print(f"Start from block {fromBlock}")
+lastBlock = w3.eth.block_number
 
 # find the last blocknumber in the database for this contract
 with engine.connect() as sql:
   for j in abi:
     if (j["type"] == "function" and j["stateMutability"] != "view") or (j["type"] == "event" and j["anonymous"] != True):
       table_name = j['table']
+      #print('this is the table name', table_name)
       sql_check_table_exists = f"""select max(block_number) from {schema}."{table_name}" """
       max_block = sql.execute(text(sql_check_table_exists)).scalar()
       if max_block != None and max_block > fromBlock:
         fromBlock = max_block + 1
-
-print(f"Start from block {fromBlock}")
-
-lastBlock = w3.eth.block_number
-
 
 # Fetch event data from each block
 while fromBlock < lastBlock:
@@ -185,35 +175,38 @@ while fromBlock < lastBlock:
   if(toBlock > lastBlock):
     toBlock = lastBlock
   print(f"Fetching events from block {fromBlock} to {toBlock}")
+  print("BlockStep:", blocksStep)
   cnt = 0
+
   # Make sure we treat block as atomic so even if it crashes, we only have a full block or non
   with sessionmaker(engine).begin() as session: 
     for address in addresses:
+      
       # TODO: manage too many results errors and manage the number of blocks automatically
-      for t in w3.eth.get_logs({'fromBlock': fromBlock, 'toBlock': toBlock, 'address': address}):
-        print(t)
+      #for t in w3.eth.get_logs({'fromBlock': fromBlock, 'toBlock': toBlock, 'address': address}):
+      for t in read_logs(address, fromBlock, toBlock): 
 
         # Check if there is an event in the existing contract's ABI for this log
         try:
           j = dict_sign[t.topics[0].hex()]
         # If there isn't, this may be a proxy contract. Check to see if we have a proxy contract/abi. If not, get it.
         except KeyError:
-          print('Topic was not found in the ABI')
+          #print('Topic was not found in the ABI. It will probably find it in the input data.')
           pass
 
-        table_name = j["table"] #Set table_name equal to the key of 'topics[0]' in the ABI (I think)
+        table_name = j["table"] 
         values = ""
-  
-        if j["type"] == "function" and j["stateMutability"] != "view": #Read functions that change blockchain state
-          # Decode the input data
 
+        #Decode the input data
+        if j["type"] == "function" and j["stateMutability"] != "view":
           try:
             inputs, params = get_function_data(t) 
             print("inputs:", inputs, "\n params:", params)
           except:
-            pass
+            print("Could not parse input data")
+            pass #raise #Should we raise an error here?
 
-          # Encode parameters
+          # Encode functions for SQL
           try:
             for idx, value in enumerate(params):
               #if IndexError, list index out of range, then it's bc/ one of your params has an additional value in it ... figure out how these should be handled
@@ -223,16 +216,19 @@ while fromBlock < lastBlock:
                 values += ", '" + str(value) +"'"
               elif isinstance(value, bytes):
                 values += ", '\\x" + value.hex() + "'"
+              #elif isinstance(value, int): #This breaks makermcd.vat. Delete it.
+              #  values += ", '\\" + value[1:] + "'"
               else:
                 values += ", " + str(value)
           except:
-            print('Could not parse', params)
-            continue #If it can't parse, just skip it. Is that okay?
-
+            print('Could not encode parameters', params)
+            print('type1', type(params[0]), 'type2', type(params[1]), 'type3', type(params[2]))
+            continue #If it can't encode it, is it okay to write it as it is?
+          
         #if j["type"] == "function" and j["stateMutability"] == "view":
         #  print("this isnt reading certain mutable functions?")
 
-        # Decode events      
+        # Encode events for SQL  
         elif j["type"] == "event" and j["anonymous"] != True:
           event_data = eth_event.decode_log(t, eth_event.get_topic_map(abi))
           
@@ -255,14 +251,63 @@ while fromBlock < lastBlock:
         sql_insert = f"""insert into {schema}."{table_name}" values ({values})"""
         print(text(sql_insert))
 
-        #session.execute(text(sql_insert)) # *** Uncomment when you want to submit or post to the database
+        session.execute(text(sql_insert)) # Uncomment when you want to submit or post to the database
 
-        cnt += 1
+        #if w3.eth.get_logs({'fromBlock': fromBlock, 'toBlock': toBlock, 'address': address}) == []:
+          #raise ValueError("Nothing returned from Infura.")
 
-  print(f"Inserted {cnt} lines into {schema}.{table_name}")
+      # Manage the blockstep automatically
+      if cnt == 0: #If there are 0 insertions in a blockstep, increase the block step
+        blocksStep += 10
+      if cnt > 30: # If there are > 30 insertions in one blockstep, decrease the blockstep.
+        blocksStep = blocksStep/2
+      cnt += 1    
+
   fromBlock = toBlock + 1 
+  # print(f"Inserted {cnt} lines into {schema}.{table_name}") # Sometimes there are no insertions when this is printed.
 
 
 #NOTES
-# 2. double check flashloan_call and other mutable functions (that they're actually reading+writing)
+# 2. double check flashloan_call (mutable ?)
 # 3. 
+
+
+'''   
+#ethereum.transactions NOTE: This does not work yet
+if addresses == []:
+  print("Getting all transaction logs") #If the address list is empty, get all transactions
+  table_name = 'transactions'
+  # Potentially run a unique function and then exit
+  
+  sql_check_table_exists = f"""select max(block_number) from {schema}."{table_name}" """
+  with engine.connect() as sql:
+    # Check if table exists. If not, create table 
+    sql_check_table_exists = f"select count(*) from information_schema.tables where table_schema = '{schema}' and table_name = '{table_name}'"
+    if sql.execute(text(sql_check_table_exists)).scalar() == 0:  
+      columns =  "block_number bigint, block_hash bytea, address bytea, log_index int, transaction_index int, transaction_hash bytea"
+      sql_create_table = f""" create table {schema}."{table_name}" ( {columns} )"""
+      print(sql_create_table, 'created table')
+      sql.execute(text(sql_create_table))
+  # Read in blocks
+  abi = []
+  print('fromblock', fromBlock)
+  toBlock = fromBlock + 1
+  # Sample response: AttributeDict({'address': '0x875773784Af8135eA0ef43b5a374AaD105c5D39e', 'blockHash': HexBytes('0x500e9107ee7757e683b8420f94897dcbee789761fe0949f5f9374c262afc8725'), 'blockNumber': 12801059, 'data': '0x00000000000000000000000000000000000000000000002a9bf0f397f1080000', 'logIndex': 0, 'removed': False, 'topics': [HexBytes('0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'), HexBytes('0x000000000000000000000000031f71b5369c251a6544c41ce059e6b3d61e42c6'), HexBytes('0x000000000000000000000000275da8e61ea8e02d51edd8d0dc5c0e62b4cdb0be')], 'transactionHash': HexBytes('0x558a88ca3fe10e02de0730844f6ae55708f0b498b9527257783be0986d17d995'), 'transactionIndex': 0})
+'''
+'''
+# Store addresses, ABIs, and contract names
+address_list, abi_list, contract_name_list, contract_list = [],[],[],[]
+
+# Get addresses, contract names, and ABI parameters. # test on makermcd.join
+for address in addresses: # This only gets the last address. Get all. I think you have to put this under 'for address in addresses' below. ***
+  address, abi, contract_name = get_abi(address, schema, contract_name)
+  address = w3.toChecksumAddress(address)
+  address_list.append(address)
+  abi_list.append(abi)
+  contract_name_list.append(contract_name)
+  #contract_list.append(w3.eth.contract(address = address, abi = abi))
+  j, dict_evt, dict_fn, dict_sign = get_abi_params(abi, contract_name, w3) # Get ABI parameters (function names, event names, etc.) 
+  create_schema(abi) # Create SQL Schema (if it doesn't already exist)
+contract = w3.eth.contract(address = address, abi = abi)
+print('addresses', address_list, 'names', contract_name_list)
+'''
--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import time
+import eth_event
 
 
 def get_abi(address, schema, contract_name):
@@ -13,10 +14,12 @@ def get_abi(address, schema, contract_name):
   except: 
       response = 'https://api.etherscan.io/api?module=contract&action=getsourcecode&address=' + address + '&apikey=M36N6D99NY4U4E1GEIYYFYIERRR1MF5S8F'
       abi = json.loads(requests.get(response).json()['result'][0]['ABI'])
-      contract_name = json.loads(requests.get(response).text)['result'][0]['ContractName'] # Get new contract name
+      #contract_name = json.loads(requests.get(response).text)['result'][0]['ContractName'] # Get new contract name
+      print("Retrieved the implementation ABI and contract name. ", contract_name)
 
   # Check if it's either a proxy address or implementation address.
   response = 'https://api.etherscan.io/api?module=contract&action=getsourcecode&address=' + address + '&apikey=M36N6D99NY4U4E1GEIYYFYIERRR1MF5S8F'
+  #print(response)
   source_code = requests.get(response).json()['result'][0]['SourceCode']
   #print(source_code)
   if 'IMPLEMENTATION_SLOT =' in source_code: 
@@ -39,10 +42,10 @@ def get_abi(address, schema, contract_name):
       response = 'https://api.etherscan.io/api?module=contract&action=getsourcecode&address=' + address + '&apikey=M36N6D99NY4U4E1GEIYYFYIERRR1MF5S8F'
       print(response)
       abi = json.loads(requests.get(response).json()['result'][0]['ABI'])
-      contract_name = json.loads(requests.get(response).text)['result'][0]['ContractName'] # Get new contract name
+      #contract_name = json.loads(requests.get(response).text)['result'][0]['ContractName'] # Get new contract name
 
     except:
-      print("This is an implementation contract. Implementation_slot in source code, but implementation contract not found")
+      print("This might be an implementation contract. Implementation_slot in source code, but implementation contract not found")
       pass
   
   else:
@@ -50,3 +53,42 @@ def get_abi(address, schema, contract_name):
 
   return address, abi, contract_name
 # Feature? Permanently save the proxy contract and address combination to SQL. Read from there first before doing the above function.
+
+
+
+#From the ABI, get function and event names j['name']
+
+# add tablename to abi json 
+dict_fn = {} ## Manage an index for disambuguation of functions with same names but different signature
+dict_evt = {} ## Manage an index for disambuguation of events  with same names but different signature
+dict_sign = {} # function /events signature to item (function or event) abi
+
+def get_abi_params(abi, contract_name, w3):
+  for j in abi:
+    if j["type"] == "function" and j["stateMutability"] != "view":
+      fn_name = j["name"].lower() 
+      signature = '{}({})'.format(j['name'],','.join([input['type'] for input in j['inputs']]))
+      # Functions signature use the 4 first bytes of the sha3 then 0
+      j["signature"] = w3.sha3(text=signature)[0:4].hex() + '00000000000000000000000000000000000000000000000000000000'
+      # print(f"{j['name']}   {signature}   {j['signature']}")
+      # If the name already exists, we add an index starting by 0 at the end of the function
+      if fn_name in dict_fn:
+        j["table"] = contract_name + "_call_" + fn_name + str(dict_fn[fn_name])
+        dict_fn[fn_name] = dict_fn[fn_name]+1
+      else:
+        j["table"] = contract_name + "_call_" + fn_name
+        dict_fn[fn_name] = 0
+      dict_sign[j["signature"]] = j
+    elif j["type"] == "event" and j["anonymous"] != True:
+      j["signature"] = eth_event.get_log_topic(j)
+      fn_name = j["name"].lower()
+      # If the name already exists, we add an index starting by 0 at the end of the function
+      if fn_name in dict_evt:
+        j["table"] = contract_name + "_evt_" + fn_name + str(dict_evt[fn_name])
+        dict_evt[fn_name] = dict_evt[fn_name]+1
+      else:
+        j["table"] = contract_name + "_evt_" + fn_name
+        dict_evt[fn_name] = 0
+      dict_sign[j["signature"]] = j
+  return j, dict_evt, dict_fn, dict_sign
+
